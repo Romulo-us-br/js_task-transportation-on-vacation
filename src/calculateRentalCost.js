4fe87d70f61ae593c8788a0b52051812f2e2b8f3
Romulo Brasil <romulop.brasil25@gmail.com>
2026-02-02 23:06:51 -0300

    result 4 passed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,25 @@
 /**
  * @param {number} days
- *
+ * 40 reais dia
+ * 50 off 1 semana
+ * 20 off 3 dias
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const cost = 40;
+  let result = days * cost;
+  const week = 7;
+  const threeDays = 3;
+  const weekDiscount = 50;
+  const daysDiscount = 20;
+
+  if (days >= week) {
+    result = result - weekDiscount;
+  } else if (days >= threeDays) {
+    result = result - daysDiscount;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
